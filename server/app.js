6e85c1ca8f919ba5f92b3e130f228c860c9541a8
Lindsay Sack <lindsay.sack@gmail.com>
2018-06-15 15:55:21 -0400

    some styling updates

--- server/app.js
+++ server/app.js
@@ -6,6 +6,39 @@ const path = require("path");
 const db = require("./models").db;
 
 const app = express();
+const csvFilePath = './public/311_Service_Requests_from_2010_to_Present.csv'
+const csv = require('csvtojson');
+csv()
+  .fromFile(csvFilePath)
+  .then(jsonObj => {
+    function groupBy(objectArray, property) {
+      return objectArray.reduce(function(acc, obj) {
+        let key = obj[property];
+        if (!acc[key]) {
+          acc[key] = [];
+        }
+        acc[key].push(obj)
+        return acc
+      }, {})
+    }
+    let groupsByBorough = groupBy(jsonObj, 'Borough');
+    let queensComplaints = groupsByBorough.QUEENS
+    let brooklynComplaints = groupsByBorough.BROOKLYN
+    let statenIslandComplaints = groupsByBorough["STATEN ISLAND"]
+    let bronxComplaints = groupsByBorough.BRONX
+    let manhattanComplaints = groupsByBorough.MANHATTAN
+    
+    
+
+
+    // let groupsByComplaint = groupBy(groupsByBorough, 'Descriptor')
+    /**
+     * [
+     * 	{a:"1", b:"2", c:"3"},
+     * 	{a:"4", b:"5". c:"6"}
+     * ]
+     */
+  });
 
 // logging and body-parsing
 app.use(volleyball);
@@ -34,8 +67,7 @@ app.use(function(err, req, res, next) {
 const port = 3000;
 app.listen(port, function() {
   console.log("The server is listening closely on port", port);
-  db
-    .sync()
+  db.sync()
     .then(function() {
       console.log("Synchronated the database");
     })
