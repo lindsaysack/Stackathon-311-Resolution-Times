906b895509197bae078fe116f3be1965ddb2857f
Lindsay Sack <lindsay.sack@gmail.com>
2018-06-16 09:44:44 -0400

    able increase bubble size with mouseover, decrease with mouseout

--- client/index.js
+++ client/index.js
@@ -1,9 +1,9 @@
 const mapboxgl = require("mapbox-gl");
 const api = require("./api");
 const buildMarker = require("./marker.js");
- 
+
 api.fetchJson().then(jsonObj => {
-  console.log(jsonObj)
+  console.log(jsonObj);
 });
 
 /*
@@ -19,7 +19,6 @@ api.fetchJson().then(jsonObj => {
   * Instantiate the Map
   */
 
-  
 // mapboxgl.accessToken = "YOUR API TOKEN HERE";
 
 // const fullstackCoords = [-74.009, 40.705] // NY
@@ -132,24 +131,22 @@ api.fetchJson().then(jsonObj => {
 //   }, {})
 // }
 
-    // let groupsByBorough = groupBy(jsonObj, 'Borough');
-    // let queensComplaints = groupsByBorough.QUEENS
-    // let brooklynComplaints = groupsByBorough.BROOKLYN
-    // let statenIslandComplaints = groupsByBorough["STATEN ISLAND"]
-    // let bronxComplaints = groupsByBorough.BRONX
-    // let manhattanComplaints = groupsByBorough.MANHATTAN
-    // console.log(manhattanComplaints)
-    // let groupsByComplaint = groupBy(groupsByBorough, 'Descriptor')
-    /**
-     * [
-     * 	{a:"1", b:"2", c:"3"},
-     * 	{a:"4", b:"5". c:"6"}
-     * ]
-     */
+// let groupsByBorough = groupBy(jsonObj, 'Borough');
+// let queensComplaints = groupsByBorough.QUEENS
+// let brooklynComplaints = groupsByBorough.BROOKLYN
+// let statenIslandComplaints = groupsByBorough["STATEN ISLAND"]
+// let bronxComplaints = groupsByBorough.BRONX
+// let manhattanComplaints = groupsByBorough.MANHATTAN
+// console.log(manhattanComplaints)
+// let groupsByComplaint = groupBy(groupsByBorough, 'Descriptor')
+/**
+ * [
+ * 	{a:"1", b:"2", c:"3"},
+ * 	{a:"4", b:"5". c:"6"}
+ * ]
+ */
 
-
-
-  var svg = d3.select("svg"),
+var svg = d3.select("svg"),
   width = +svg.attr("width"),
   height = +svg.attr("height");
 
@@ -157,83 +154,155 @@ var format = d3.format(",d");
 
 var color = d3.scaleOrdinal(d3.schemeCategory20c);
 
-var pack = d3.pack()
+var pack = d3
+  .pack()
   .size([width, height])
   .padding(1.5);
 
-//code to use when ready to parse actual data. this calculates the time to resolve each issue in a row in the dataset and sets the time to the 'value for that date'   
-d3.csv("311_Service_Requests_from_2010_to_Present.csv", function (d) {
-  let createdDate = new Date(d["Created Date"])
-  let closedDate = new Date(d["Closed Date"])
-  let resolutionTime = (closedDate - createdDate)
+//code to use when ready to parse actual data. this calculates the time to resolve each issue in a row in the dataset and sets the time to the 'value for that date'
+d3.csv(
+  "311_Service_Requests_from_2010_to_Present.csv",
+  function(d) {
+    let createdDate = new Date(d["Created Date"]);
+    let closedDate = new Date(d["Closed Date"]);
+    let resolutionTime = closedDate - createdDate;
 
-  d.value = resolutionTime
-  //convert the milliseconds elapsed between issue open/close into string with days and minutes
-  function msToTime(duration) {
-    var minutes = parseInt((duration / (1000 * 60)) % 60)
-      , hours = parseInt((duration / (1000 * 60 * 60)) % 24);
+    d.value = resolutionTime;
+    //convert the milliseconds elapsed between issue open/close into string with days and minutes
+    function msToTime(duration) {
+      var minutes = parseInt((duration / (1000 * 60)) % 60),
+        hours = parseInt((duration / (1000 * 60 * 60)) % 24);
 
-    hours = (hours < 10) ? "0" + hours : hours;
-    minutes = (minutes < 10) ? "0" + minutes : minutes;
-    return hours + " hour(s) " + minutes + " minutes"
+      hours = hours < 10 ? "0" + hours : hours;
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      return hours + " hour(s) " + minutes + " minutes";
+    }
+
+    d.durationText = msToTime(resolutionTime);
+
+    if (d.value) return d;
+  },
+  function(error, classes) {
+    if (error) throw error;
+
+    var root = d3
+      .hierarchy({ children: classes })
+      .sum(function(d) {
+        return d.value;
+      })
+      .each(function(d) {
+        if ((Descriptor = d.data["Descriptor"])) {
+          d.descriptor = d.data["Descriptor"];
+        }
+      });
+
+    var node = svg
+      .selectAll(".node")
+      .data(pack(root).leaves())
+      .enter()
+      .append("g")
+      .attr("class", "node inactive")
+      .attr("transform", function(d) {
+        return "translate(" + d.x + "," + d.y + ")";
+      })
+      .on("mouseover", handleMouseOver 
+        // function() {
+        // console.log("DATA", d3.select(this));
+        // d3.select(this).attr("class", "active");}
+    )
+      .on("mouseout", handleMouseOut 
+      // function() {
+        // d3.select(this).attr("class", "inactive");}
+      );
+
+    node
+      .append("circle")
+      .attr("descriptor", function(d) {
+        return d.descriptor;
+      })
+      .attr("r", function(d) {
+        return d.r;
+      })
+      //d.descriptor is the description of the type of complaint, all complaints with the same descriptor will have the same color
+      .style("fill", function(d) {
+        return color(d.descriptor);
+      });
+
+    node
+      .append("clipPath")
+      .attr("id", function(d) {
+        return "clip-" + d.descriptor;
+      })
+      .append("use")
+      .attr("href", function(d) {
+        return "#" + d.descriptor;
+      });
+
+    node
+      .append("text")
+      .attr("clip-path", function(d) {
+        return "url(#clip-" + d.descriptor + ")";
+      })
+      .selectAll("tspan")
+      .data(function(d) {
+        return d.descriptor.split(/(?=[A-Z][^A-Z])/g);
+      })
+      .enter()
+      .append("tspan")
+      .attr("x", 0)
+      .attr("y", function(d, i, nodes) {
+        return 13 + (i - nodes.length / 2 - 0.5) * 10;
+      })
+      .text(function(d) {
+        return d;
+      });
+
+    node
+      .append("div")
+      .attr("class", "tooltip")
+      .text(function(d) {
+        //adds pop up for duration when hovering over bubble
+        return d.data["Borough"] + "\n" + d.data.durationText;
+      })
+      .style("opacity", 0);
   }
+);
 
-  d.durationText = msToTime(resolutionTime)
+function handleMouseOver(d, i) {
+  // Add interactivity
 
-  if (d.value) return d;
-}, function (error, classes) {
-  if (error) throw error;
-
-  var root = d3.hierarchy({ children: classes })
-    .sum(function (d) { return d.value; })
-    .each(function (d) {
-      if (Descriptor = d.data["Descriptor"]) {
-        d.descriptor = d.data["Descriptor"]
-      }
-    });
-
-  var node = svg.selectAll(".node")
-    .data(pack(root).leaves())
-    .enter().append("g")
-    .attr("class", "node inactive")
-    .attr("transform", function (d) { return "translate(" + d.x + "," + d.y + ")"; })
-    .on("mouseover", function() {
-        console.log("DATA", d3.select(this))
-        d3.select(this)
-          .attr("class", "active")
+  // Use D3 to select element, change color and size
+  d3.select(this).select("circle")
+  .attr("descriptor", "orange")
+    .attr("r", function() {
+      // d.r = d.r * 2
+      return d.r * 1.2
     })
-    .on("mouseout",  function() {  
-      d3.select(this)
-          .attr("class", "inactive")
-    });
 
-  node.append("circle")
-    .attr("descriptor", function (d) {
-      return d.descriptor;
-    })
-    .attr("r", function (d) { return d.r; })
-    //d.descriptor is the description of the type of complaint, all complaints with the same descriptor will have the same color
-    .style("fill", function (d) { return color(d.descriptor); });
+  // Specify where to put label of text
+  // svg
+  //   .append("text")
+  //   .attr({
+  //     id: "t" + d.x + "-" + d.y + "-" + i, // Create an id for text so we can select it later for removing on mouseout
+  //     x: function() {
+  //       return xScale(d.x) - 30;
+  //     },
+  //     y: function() {
+  //       return yScale(d.y) - 15;
+  //     }
+  //   })
+  //   .text(function() {
+  //     return d.data["Borough"] + "\n" + d.data.durationText; // Value of the text
+  //   });
+}
 
-  node.append("clipPath")
-    .attr("id", function (d) { return "clip-" + d.descriptor; })
-    .append("use")
-    .attr("href", function (d) { return "#" + d.descriptor; });
-
-  node.append("text")
-    .attr("clip-path", function (d) { return "url(#clip-" + d.descriptor + ")"; })
-    .selectAll("tspan")
-    .data(function (d) { return d.descriptor.split(/(?=[A-Z][^A-Z])/g); })
-    .enter().append("tspan")
-    .attr("x", 0)
-    .attr("y", function (d, i, nodes) { return 13 + (i - nodes.length / 2 - 0.5) * 10; })
-    .text(function (d) { return d; });
-
-  let div = node.append("div")
-    .attr('class', "tooltip")
-    .text(function (d) {
-      //adds pop up for duration when hovering over bubble
-      return d.data["Borough"] + "\n" + (d.data.durationText);
-    })
-    .style("opacity", 0);
-});
+function handleMouseOut(d, i) {
+  // Use D3 to select element, change color back to normal
+  d3.select(this).select("circle")
+  .attr("fill", "black")
+  .attr("r", function() {
+    return d.r  
+  })
+  // Select text by id and then remove
+  // d3.select("#t" + d.x + "-" + d.y + "-" + i).remove(); // Remove text location
+}
