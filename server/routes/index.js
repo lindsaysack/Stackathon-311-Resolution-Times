7b5eeae248cd9fd6385ddf83603fac312e170f8e
Lindsay Sack <lindsay.sack@gmail.com>
2018-06-17 23:41:18 -0400

    udpated procfile and removed get route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,18 +1,18 @@
 const router = require("express").Router();
 const csv = require('csvtojson'); 
 
-router.get("/", (req, res, next) => {
-  const csvFilePath = '../../public/311_Service_Requests_from_2010_to_Present.csv'
-  // console.log("dsafsdfaswecdas", csvFilePath)
-  csv()
-    .fromFile(csvFilePath)
-    .then(jsonObj => {
-      console.log(jsonObj)
-      res.json("hey", jsonObj)
+// router.get("/", (req, res, next) => {
+//   const csvFilePath = '../../public/311_Service_Requests_from_2010_to_Present.csv'
+//   // console.log("dsafsdfaswecdas", csvFilePath)
+//   csv()
+//     .fromFile(csvFilePath)
+//     .then(jsonObj => {
+//       console.log(jsonObj)
+//       res.json("hey", jsonObj)
       
-    })
-    .catch(next)
-  }
-)
+//     })
+//     .catch(next)
+//   }
+// )
 
 module.exports = router;
